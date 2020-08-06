05c527b249f52a3e481f48f6ef89e4bbdd206785
Vinicius Rosa <vinicius.rosa00@gmail.com>
2020-08-06 00:26:54 -0300

    create class done

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 app.use(express.json());
+app.use(routes)
 
-app.get('/users', (request, response) => {
-
-})
 
 app.listen(3333, () => {
     console.log('run');
