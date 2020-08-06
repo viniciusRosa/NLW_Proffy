110f872447ca291c099c79895c6901c9d10645f5
Vinicius Rosa <vinicius.rosa00@gmail.com>
2020-08-05 23:15:32 -0300

    database install

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,11 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
+
+app.get('/users', (request, response) => {
+
+})
 
 app.listen(3333, () => {
     console.log('run');
